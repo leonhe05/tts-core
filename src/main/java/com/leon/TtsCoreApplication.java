fe545ad40a4f8c52374114a9ec08b60987f4ad12
heyaxing <1123640216@qq.com>
2025-04-18 23:18:05 +0800

    feat: 日志增加机器码；健康检查

--- src/main/java/com/leon/TtsCoreApplication.java
+++ src/main/java/com/leon/TtsCoreApplication.java
@@ -1,14 +1,18 @@
 package com.leon;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @MapperScan("com.leon.infrastructure.mapper")
 public class TtsCoreApplication {
 
     public static void main(String[] args) {
+        MDC.put("parentId", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
         SpringApplication.run(TtsCoreApplication.class, args);
     }
 
