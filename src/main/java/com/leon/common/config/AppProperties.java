6c3f6f0dce5c79aa0f682a77da7799bc0a0bc4a8
heyaxing <heyaxing@xgd.com>
2025-04-24 11:34:24 +0800

    feat: 随机key

--- src/main/java/com/leon/common/config/AppProperties.java
+++ src/main/java/com/leon/common/config/AppProperties.java
@@ -3,6 +3,7 @@ package com.leon.common.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -16,4 +17,8 @@ public class AppProperties {
 
     private String cUid = "8asdjkl13789asdf1238dj24k378";
 
+    public String getRandomBaiduApiKey() {
+        Collections.shuffle(baiduApiKey);
+        return baiduApiKey.get(0);
+    }
 }
